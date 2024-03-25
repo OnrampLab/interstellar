40317683832e6339cb162638671fbae315063865
Kos Huang <koshuang@gmail.com>
2024-03-25 15:04:48 +0800

    chore(framework): [handle_ui_error] new

--- src/transstellar/framework/__init__.py
+++ src/transstellar/framework/__init__.py
@@ -7,6 +7,7 @@ __all__ = [
     "ConfigService",
     "Element",
     "handle_error",
+    "handle_ui_error",
     "Loggable",
     "Logger",
     "MainConfig",
@@ -20,6 +21,7 @@ from .base_ui_test import BaseUITest
 from .config_service import ConfigService
 from .element import Element
 from .handle_error import handle_error
+from .handle_ui_error import handle_ui_error
 from .loggable import Loggable
 from .logger import Logger
 from .main_config import MainConfig
