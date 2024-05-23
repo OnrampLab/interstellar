b9d1bf326dbd1424a8fa026367c32696a79af763
Kos Huang <koshuang@gmail.com>
2024-05-23 14:45:56 +0800

    test(api_client): [APIClient] test with GitHub API

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A pytest framework
 
+## Prepare testing environment
+
+1. Docker
+
+2. Create a GitHub personal access token with permission to access transstellar and permission to read and write issue.
+
+Update .env `GITHUB_PERSONAL_ACCESS_TOKEN` variable.
+
+
 ## How to publish to PyPI
 
 1. Update PyPI Token. Open .env to update `POETRY_PYPI_TOKEN_PYPI` variable.
--- tests/unit/api_client/test_api_client.py
+++ tests/unit/api_client/test_api_client.py
@@ -1,8 +1,74 @@
+import os
+from datetime import datetime
+
 from transstellar.api_client import APIClient
+from transstellar.framework.base_test import BaseTest
 
 
-class TestAPIClient:
-    def test_api_client(self):
-        api_client = APIClient("https://api.github.com")
+class TestAPIClient(BaseTest):
+    def setup_method(self):
+        self.token = os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN")
+        self.api_client = APIClient("https://api.github.com")
+        self.api_client.as_token(self.token)
+        self.comment_id = self.create_comment()
+        self.should_delete_comment = True
 
-        pass
+    def teardown_method(self):
+        if self.should_delete_comment:
+            self.delete_comment()
+
+    def create_comment(self):
+        payload = {"body": f"test comment - {datetime.now()}"}
+        json = self.api_client.post(
+            "repos/OnrampLab/transstellar/issues/1/comments", payload
+        )
+
+        assert json["body"].startswith("test comment - ")
+
+        return json["id"]
+
+    def delete_comment(self):
+        self.api_client.delete(
+            f"repos/OnrampLab/transstellar/issues/comments/{self.comment_id}"
+        )
+
+    def test_as_token(self):
+        assert self.api_client.token is not None
+
+    def test_post(self):
+        assert self.comment_id is not None
+
+    def test_get(self):
+        json = self.api_client.get(
+            f"repos/OnrampLab/transstellar/issues/comments/{self.comment_id}"
+        )
+
+        assert json["id"] == self.comment_id
+
+    def test_patch(self):
+        payload = {"body": f"test updating comment - {datetime.now()}"}
+        json = self.api_client.patch(
+            f"repos/OnrampLab/transstellar/issues/comments/{self.comment_id}",
+            payload,
+        )
+        assert json["body"].startswith("test updating comment - ")
+
+    def test_put(self):
+        payload = {"labels": ["enhancement"]}
+        labels_json = self.api_client.put(
+            "repos/OnrampLab/transstellar/issues/1/labels",
+            payload,
+        )
+
+        assert labels_json[0]["name"] == "enhancement"
+
+        payload = {"labels": []}
+        self.api_client.put(
+            "repos/OnrampLab/transstellar/issues/1/labels",
+            payload,
+        )
+
+    def test_delete(self):
+        self.delete_comment()
+
+        self.should_delete_comment = False
