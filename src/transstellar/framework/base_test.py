dce1848d224aade7dc74b7853e3b84a45cd0295a
Kos Huang <koshuang@gmail.com>
2024-06-17 17:01:35 +0800

    chore(framework): [BaseTest] add sleep()

--- src/transstellar/framework/base_test.py
+++ src/transstellar/framework/base_test.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from .application import Application
@@ -12,3 +14,8 @@ class BaseTest:
     def setup_method_base_test(self, app: Application):
         self.app = app
         self.logger = Logger(self.__class__.__name__)
+
+    def sleep(self, seconds: float):
+        self.logger.debug(f"sleep: {seconds} seconds")
+
+        time.sleep(seconds)
